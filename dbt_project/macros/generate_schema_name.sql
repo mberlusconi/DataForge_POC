389671ff3f1546cfaf576fce50d585fbb9cd79bf
Martin Berlusconi <martin.berlusconi@allata.com>
2026-09-14 15:13:00 -0300

    feat(dbt): update generate_schema_name macro for UAT/PROD dynamic schema replacement

--- dbt_project/macros/generate_schema_name.sql
+++ dbt_project/macros/generate_schema_name.sql
@@ -3,7 +3,24 @@
     {%- if custom_schema_name is none -%}
         {{ target.schema }}
     {%- else -%}
-        {{ custom_schema_name | trim }}
+        
+        {# 1. Slim CI for PRs #}
+        {%- if target.name == 'ci' -%}
+            {{ custom_schema_name | trim }}_{{ env_var('PR_SCHEMA') }}
+
+        {# 2. CD Deployment to UAT #}
+        {%- elif target.name == 'uat' -%}
+            {{ custom_schema_name | trim | replace('_DEV', '_UAT') }}
+
+        {# 3. CD Deployment to PROD #}
+        {%- elif target.name == 'prod' -%}
+            {{ custom_schema_name | trim | replace('_DEV', '_PROD') }}
+
+        {# 4. Local execution, Dagster, Terraform, and DEV target (keeps original names) #}
+        {%- else -%}
+            {{ custom_schema_name | trim }}
+        {%- endif -%}
+
     {%- endif -%}
 
 {%- endmacro %}
